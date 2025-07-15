f8e0210f97e527902fa181b82ebd6d428b1d35c0
Donny <107092000+Donny-Guo@users.noreply.github.com>
2025-07-14 17:14:33 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a>WeatherDash</a>
+        <a>Dashboard</a>
+        <a>Search</a>
+        <a href="https://youtube.com">About</a>
+      </header>
+    </main>
+  )
+}
+
+export default App
